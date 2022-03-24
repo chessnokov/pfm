5639e6c0f5f2459abfe784acdb05d4dfeaa1f50b
Andrey Chesnokov <chessnokov@gmail.com>
2022-03-24 10:55:34 +0300

    common const

--- bin/pfm.rs
+++ bin/pfm.rs
@@ -10,6 +10,7 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 use cortex_m_rt::entry;
 use pfm as _;
 
+// LD2
 const LED: u32 = 7;
 
 #[entry]
@@ -23,7 +24,7 @@ fn main() -> ! {
     let pins = p.GPIOB;
 
     let clear_mask_one_bit: u32 = 0xffffffff ^ (0b1 << LED);
-    let clear_mask_two_bit: u32 = 0xffffffff ^ (0b11 << 14);
+    let clear_mask_two_bit: u32 = 0xffffffff ^ (0b11 << (LED * 2));
 
     // Set OSPEEDR to low speed for PB7(LD2)
     pins.ospeedr
@@ -31,27 +32,27 @@ fn main() -> ! {
 
     // Set OTYPER to b0 for PB7(LD2)
     pins.otyper
-        .modify(|r, w| unsafe { w.bits(r.bits() & clear_mask_one_bit | (0b1 << 7)) });
+        .modify(|r, w| unsafe { w.bits(r.bits() & clear_mask_one_bit | (0b1 << LED)) });
 
     pins.pupdr
         .modify(|r, w| unsafe { w.bits(r.bits() & clear_mask_two_bit) });
 
     // Set MODE to b01 for PB7(LD2)
     pins.moder
-        .modify(|r, w| unsafe { w.bits(r.bits() & clear_mask_two_bit | (0b01 << 14)) });
+        .modify(|r, w| unsafe { w.bits(r.bits() & clear_mask_two_bit | (0b01 << (LED * 2))) });
 
     hprintln!("Hello, world!").unwrap();
 
     loop {
         pins.odr
             .modify(|r, w| unsafe { w.bits(r.bits() & clear_mask_one_bit) });
-        pins.bsrr.write(|w| unsafe { w.bits(1 << 7) });
+        pins.bsrr.write(|w| unsafe { w.bits(1 << LED) });
         hprintln!("-").unwrap();
         delay.delay_ms(1000);
 
         pins.odr
-            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << 7)) });
-        pins.bsrr.write(|w| unsafe { w.bits(1 << 7) });
+            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << LED)) });
+        pins.bsrr.write(|w| unsafe { w.bits(1 << LED) });
         hprintln!("+").unwrap();
         delay.delay_ms(1000);
     }
